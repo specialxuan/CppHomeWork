00a090d5812dce2e0f9f49cedd87dccb7bb212f4
Special.Xuan <special.xuan@outlook.com>
2021-09-24 11:32:22 +0800

    update par.cpp

--- par.cpp
+++ par.cpp
@@ -7,25 +7,25 @@ double f(double x)
 }
 int main(int argc, char *argv[])
 {
-    int myid, numprocs;
+    int myId, numprocs;
     int n, i;
-    double mypi, pi;
+    double myPi, pi;
     double h, sum, x;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-    printf("Process %d of %d.\n", myid, numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myId);
+    printf("Process %d of %d.\n", myId, numprocs);
     n = 100;
     h = 1.0 / (double)n;
     sum = 0.0;
-    for (i = myid + 1; i <= n; i += numprocs)
+    for (i = myId + 1; i <= n; i += numprocs)
     {
         x = h * ((double)i - 0.5);
         sum += f(x);
     }
-    mypi = h * sum;
-    MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (myid == 0)
+    myPi = h * sum;
+    MPI_Reduce(&myPi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (myId == 0)
     {
         printf("The result is %.10f.\n", pi);
     }
