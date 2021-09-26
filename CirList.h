910b7cf485f683822a54379cb1400c2e52ff1192
Special.Xuan <special.xuan@outlook.com>
2021-09-26 20:17:54 +0800

    update CirList.h

--- CirList.h
+++ CirList.h
@@ -21,17 +21,15 @@ protected:
 
 public:
     CirList() : Head(NULL), Tail(NULL), Cur(NULL),zero(0), Count(0) {}
-    ~CirList() { makeEmpty(); }
+    ~CirList() { Clear(); }
     int insert(const T &it, int pos = 0);
     int append(const T &it) { return insert(it, Count); }
     int remove(T &it, int pos = 0);
-    bool data(T &it, int pos = -1);
-    bool nextData(T &it);
-    bool setData(const T &it, int pos = 0);
     T &operator[](int pos);
     const T &operator[](int pos) const;
     bool isEmpty() { return Count == 0; }
-    void makeEmpty();
+    void Clear();
+    int length() const { return Count; }
 };
 
 template <class T>
@@ -49,7 +47,7 @@ int CirList<T>::insert(const T &it, int pos)
             Tail = Head; //只有一个节点
         Tail->next = Head;
     }
-    else if (pos == Count)
+    else if (pos == Count - 1)
     { // 数据插入到尾部
         Tail->next = new Node<T>(it, Head);
         Tail = Tail->next;
@@ -71,6 +69,7 @@ int CirList<T>::insert(const T &it, int pos)
         Cur = new Node<T>(it, p->next);
         p->next = Cur;
     }
+
     return Count;
 }
 
@@ -91,7 +90,7 @@ int CirList<T>::remove(T &it, int pos)
     }
     else
     { //移除其它节点
-        for (; pos > 1; --pos)
+        for (int i = 1; i < pos; i++)
             Cur = Cur->next;
         p = Cur->next;
         Cur->next = p->next;
@@ -103,46 +102,8 @@ int CirList<T>::remove(T &it, int pos)
     Count--;
     if (Count == 0) //表空
         Tail = Head = Cur = 0;
-}
 
-template <class T>
-bool CirList<T>::data(T &it, int pos)
-{
-    if (Count == 0 || pos < -1 || pos >= Count)
-        return false;
-    if (pos >= 0)
-    {
-        Cur = Head;
-        for (; pos > 0; --pos)
-            Cur = Cur->next;
-    }
-    data = Cur->data;
-    return true;
-}
-
-template <class T>
-bool CirList<T>::nextData(T &it)
-{
-    if (Count == 0)
-        return false;
-    Cur = Cur->next;
-    it = Cur->data;
-    return true;
-}
-
-template <class T>
-bool CirList<T>::setData(const T &it, int pos)
-{
-    if (pos < -1 || pos >= Count || Count == 0)
-        return false;
-    if (pos >= 0)
-    {
-        Cur = Head;
-        for (; pos > 0; --pos)
-            Cur = Cur->next;
-    }
-    Cur->data = it;
-    return true;
+    return Count;
 }
 
 template <class T>
@@ -162,7 +123,7 @@ T &CirList<T>::operator[](int pos)
 }
 
 template <class T>
-void CirList<T>::makeEmpty()
+void CirList<T>::Clear()
 {
     while (Count--)
     {
@@ -170,6 +131,6 @@ void CirList<T>::makeEmpty()
         Head = Head->next;
         delete Cur;
     }
-    Cur = Head = Tail = 0;
+    Cur = Head = Tail = NULL;
     Count = 0;
 }
--- hm2.cpp
+++ hm2.cpp
@@ -1,22 +1,37 @@
 #include "CirList.h"
 
+void output(CirList<int> &cl)
+{
+    for (int i = 0; i < cl.length(); i++)
+    {
+        cout << cl[i] << ",";
+    }
+    cout << "end\n";
+}
+
 int main ()
 {
-    CirList<int> cl1, cl2, cl3;
+    CirList<int> cl1;
     cl1.insert(1, 0);
     cl1.insert(3, 1);
     cl1.insert(2, 1);
     cl1.insert(2, 1);
-    cout << cl1[0] << "\n";
-    cout << cl1[1] << "\n";
-    cout << cl1[2] << "\n";
-    cout << cl1[3] << "\n";
+    output(cl1);
     cl1[2] = 3;
     cl1[3] = 4;
-    cout << cl1[0] << "\n";
-    cout << cl1[1] << "\n";
-    cout << cl1[2] << "\n";
-    cout << cl1[3] << "\n";
+    output(cl1);
+    cout << cl1[-1] << "\n";
+
+    int rm;
+
+    cout << "Count = " << cl1.remove(rm, 1) << "\n";
+    cout << rm << "\n";
+    output(cl1);
+
+    cl1.insert(5, 3);
+
+    cl1.append(5);
+    output(cl1);
 
     return 0;
 }
