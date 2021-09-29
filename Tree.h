4c4182e6b4a46b8bb04884d31f2287e1e7676e3e
Special.Xuan <special.xuan@outlook.com>
2021-09-29 10:02:48 +0800

    update Tree.h

--- Tree.h
+++ Tree.h
@@ -1,24 +1,24 @@
 #include <iostream>
 
 template <class T>
-class BinTreeNode
+class BiTreeNode
 {
 protected:
-    BinTreeNode<T> *Left;
-    BinTreeNode<T> *Right;
+    BiTreeNode<T> *Left;
+    BiTreeNode<T> *Right;
     T Data;
 
 public:
-    BinTreeNode() : Left(NULL), Right(NULL) {}
-    BinTreeNode(const T &item,
-                BinTreeNode<T> *l = NULL,
-                BinTreeNode<T> *r = NULL)
-        : Data(item), Left(l), Right(r) {}
-    ~BinTreeNode();
+    BiTreeNode() : Left(NULL), Right(NULL) {}
+    BiTreeNode(const T &item,
+               BiTreeNode<T> *l = NULL,
+               BiTreeNode<T> *r = NULL)
+        : Left(l), Right(r), Data(item) {}
+    ~BiTreeNode();
 };
 
 template <class T>
-BinTreeNode<T>::~BinTreeNode()
+BiTreeNode<T>::~BiTreeNode()
 {
 }
 
@@ -26,29 +26,27 @@ template <class T>
 class BinaryTree
 {
 private:
-    BinTreeNode<T> *Root;
-    void createBinTree(ifstream &in, BinTreeNode<T> *&t);
-    BinTreeNode<T> *Parent(
-        BinTreeNode<T> *Tree,
-        BinTreeNode<T> *t);
-    int insert(BinTreeNode<T> *&Tree, const T &item);
-    void tranerse(BinTreeNode<T> *tree);//TODO:
-    void destroy(BinTreeNode<T> *tree);
+    BiTreeNode<T> *Root;
+
+    void createBinTree(ifstream &in, BiTreeNode<T> *&t);
+    BiTreeNode<T> *Parent(BiTreeNode<T> *Tree, BiTreeNode<T> *t);
+    int insert(BiTreeNode<T> *&Tree, const T &item);
+    void tranerse(BiTreeNode<T> *tree); //TODO:
+    void destroy(BiTreeNode<T> *tree);
 
 public:
     BinaryTree(/* args */);
     virtual BinaryTree() : Root(NULL);
     virtual ~BinaryTree(){destroy(Root)};
-    virtual bool isEmpty(){return} //TODO:
-    virtual BinTreeNode<T> *find(const T &item) const;
-    BinTreeNode<T> *root() const {return Root};
+    virtual bool isEmpty() { return } //TODO:
+    virtual BiTreeNode<T> *find(const T &item) const;
+    BiTreeNode<T> *root() const {return Root};
     friend istream &operator>>(istream &in, BInarytree<T> &t);
     friend istream &operator<<(istream &out, BInarytree<T> &t);
-    
 };
 
 template <class T>
-void BinaryTree<T>::destroy(BinTreeNode<T> *t)
+void BinaryTree<T>::destroy(BiTreeNode<T> *t)
 {
     if (T)
     {
